e88aea6a863020b2d8be0ca0574127e2407314e1
acaciopb@hotmail.com <acaciopb@hotmail.com>
2021-01-07 11:17:38 -0300

    half progress

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculadora from './main/Calculadora';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Calculadora</h1>
+  <Calculadora></Calculadora>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/main/Calculadora.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './Calculadora.css'
+
+import button from '../components/button'
+
+export default class Calculadora extends Component {
+
+    render() {
+        return (
+            <div className="Calculadora">
+                
+                <input type="submit" value="AC"></input>
+                <input type="submit" value="/"></input>
+                <input type="submit" value="7"></input>
+                <input type="submit" value="8"></input>
+                <input type="submit" value="9"></input>
+                <input type="submit" value="*"></input>
+                <input type="submit" value="4"></input>
+                <input type="submit" value="5"></input>
+                <input type="submit" value="6"></input>
+                <input type="submit" value="-"></input>
+                <input type="submit" value="2"></input>
+                <input type="submit" value="3"></input>
+                <input type="submit" value="+"></input>
+                <input type="submit" value="0"></input>
+                <input type="submit" value="."></input>
+                <input type="submit" value="="></input>
+
+
+                
+
+            </div>
+        )
+    }
+}
